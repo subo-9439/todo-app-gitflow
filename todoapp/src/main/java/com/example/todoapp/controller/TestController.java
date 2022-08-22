634e5c5baf386399778f7eb97be158d94c7ff5d8
subo-9439 <kws3363@gmail.com>
2022-08-22 16:17:57 +0900

    <add>:<To-do Application!>
    해당 문자 표시하
    <issue-number>

---
+++ todoapp/src/main/java/com/example/todoapp/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.todoapp.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+ 
+  @GetMapping("/todo")
+  public String getTodo(){
+    String str = "To-do Application!";
+    System.out.println(str);
+    return str;
+  }
+}
